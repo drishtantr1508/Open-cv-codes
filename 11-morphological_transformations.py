2b52dec635aa3fea62d18299c982dcd823e19f3b
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2020-12-14 20:33:05 +0530

    ...

--- 11-morphological_transformations.py
+++ 11-morphological_transformations.py
@@ -7,19 +7,33 @@ _, mask = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY_INV)
 
 kernal = np.ones((5, 5), np.uint8)
 
+# It increses the size of image by a slight at thr borders...
 dilation = cv2.dilate(mask, kernal, iterations=2)
+
+# It decreases the size by a slight at the border.
 erosion = cv2.erode(mask, kernal, iterations=1)
+
+# Erosion followed by dilation
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+
+# Dilation followed by erosion.
 closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+
+# Difference between dilation and erosion of an image.
 mg = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernal)
+
+# Difference between opening and input image.
 th = cv2.morphologyEx(mask, cv2.MORPH_TOPHAT, kernal)
 
-titles = ['image', 'mask', 'dilation',
-          'erosion', 'opening', 'closing', 'mg', 'th']
-images = [img, mask, dilation, erosion, opening, closing, mg, th]
+# Difference between closing and input image.
+bh = cv2.morphologyEx(mask,  cv2.MORPH_BLACKHAT, kernal)
 
-for i in range(8):
-    plt.subplot(2, 4, i+1), plt.imshow(images[i], 'gray')
+titles = ['image', 'mask', 'dilation',
+          'erosion', 'opening', 'closing', 'Morph Gradient', 'Top Hat', 'Black Hat']
+images = [img, mask, dilation, erosion, opening, closing, mg, th, bh]
+
+for i in range(len(titles)):
+    plt.subplot(3, 3, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 
